dda5a01b784c5b41140cea673c149d95966773b6
Dayne-Game <daynegame024@gmail.com>
2019-06-17 09:11:50 +1200

    Added a COntact Form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,9 +8,29 @@ import "../CSS/contact.css"
 const ContactPage = () => (
     <Layout>
         <div className="wrapper">
-            <form name="contact" method="post" data-netlify="true" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input name="name-field" placeholder="Your Name" type="text"></input>
-                <button>Send</button>
+            <form
+                name="news-post"
+                method="post"
+                data-netlify="true"
+            >
+                <p>
+                    <label>Your Name: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Your Email: <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <label>Your Role: <select name="role[]" multiple>
+                        <option value="leader">Leader</option>
+                        <option value="follower">Follower</option>
+                    </select></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
             </form>
         </div>
     </Layout>
